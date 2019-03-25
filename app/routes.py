46aa194e8eade34454c95e4b34d86b6f84e057f1
hardik-uppal <hardikuppal.hu@gmail.com>
2019-03-24 23:16:38 -0400

    pie chart fixed

--- app/routes.py
+++ app/routes.py
@@ -23,13 +23,18 @@ def index():
         users_chart1.append(dt_chart1[i][1])
     
     #chart2
-    comment_count_chart2=[]
-    nlcLabel_chart2=[]
+    comment_count_chart2=[0]*5
+#    nlcLabel_chart2=[None]*5
+    nlcLabel=['Identity Hate','Neutral', 'Obscene','Threat','Toxic']
     dt_chart2=ExecuteReader("select count(*) as count,NlcLabel from Comments Group by NlcLabel order by NlcLabel")
+#    print(dt_chart2)
     for i in range(len(dt_chart2)):
-        comment_count_chart2.append(dt_chart2[i][0])
-        nlcLabel_chart2.append(dt_chart2[i][1])  
-    
+#        comment_count_chart2.append(dt_chart2[i][0])
+#        nlcLabel_chart2.append(dt_chart2[i][1])  
+        for j in range(len(nlcLabel)):
+            if nlcLabel[j]==dt_chart2[i][1]:
+                comment_count_chart2[j]=dt_chart2[i][0]
+            
 
     #titlecount
     count_media_id=ExecuteReader("select count(distinct media_id) from Comments")
@@ -57,4 +62,4 @@ def index():
         count_explicit_graph.append(dt_graph[i][0])
         
     
-    return render_template('index.html',users=users_chart1, comment_count=comment_count_chart1,categories=nlcLabel_chart2,cat_comment_count=comment_count_chart2,count_explicit_graph=count_explicit_graph,date_graph=date_graph,count_media_id=count_media_id[0][0],explicit_comments=explicit_comments[0][0])
+    return render_template('index.html',users=users_chart1, comment_count=comment_count_chart1,categories=nlcLabel,cat_comment_count=comment_count_chart2,count_explicit_graph=count_explicit_graph,date_graph=date_graph,count_media_id=count_media_id[0][0],explicit_comments=explicit_comments[0][0])
