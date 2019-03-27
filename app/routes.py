2cdcdca622b5afad53d4bd1df22f41198553eaa7
hardik-uppal <hardikuppal.hu@gmail.com>
2019-03-26 23:55:04 -0400

    updated to 30 sec get call reload

--- app/routes.py
+++ app/routes.py
@@ -101,7 +101,7 @@ def ExtractToken():
                 logfile.close()
             GetCall(accessToken)
             scheduler = BackgroundScheduler()
-            scheduler.add_job(lambda: GetCall(accessToken), trigger="interval", seconds=100)
+            scheduler.add_job(lambda: GetCall(accessToken), trigger="interval", seconds=30)
             scheduler.start()
 #            GetCall(session['accessToken'])
     return render_template('accessToken.html')
